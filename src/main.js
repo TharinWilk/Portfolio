1df5d6edc9edbd33d4fb7ac02c2ed5c1102d7b14
Tharin Wilk <Tharin.m.wilk@gmail.com>
2023-03-23 18:59:46 -0400

    2.0.1 Installed & Activated Pinia to the project

--- src/main.js
+++ src/main.js
@@ -1,11 +1,39 @@
-import { createApp } from 'vue'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faLinkedin, faHtml5, faCss3, faJsSquare, faSass, faVuejs, faBootstrap, faGulp } from '@fortawesome/free-brands-svg-icons'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faLinkedin,
+  faHtml5,
+  faCss3,
+  faJsSquare,
+  faSass,
+  faVuejs,
+  faBootstrap,
+  faGulp,
+} from "@fortawesome/free-brands-svg-icons";
+import App from "./App.vue";
+import router from "./router";
 
-library.add(faGithub, faLinkedin, faEnvelope, faHtml5, faCss3, faJsSquare, faSass, faVuejs, faBootstrap, faGulp )
+const pinia = createPinia();
 
-createApp(App).use(router).component('fa', FontAwesomeIcon).mount('#app')
+library.add(
+  faGithub,
+  faLinkedin,
+  faEnvelope,
+  faHtml5,
+  faCss3,
+  faJsSquare,
+  faSass,
+  faVuejs,
+  faBootstrap,
+  faGulp
+);
+
+createApp(App)
+  .use(router)
+  .use(pinia)
+  .component("fa", FontAwesomeIcon)
+  .mount("#app");
