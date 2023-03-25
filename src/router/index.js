2232f1eec95c2869222f55e4ac1ad24a8e84803c
Tharin Wilk <Tharin.m.wilk@gmail.com>
2023-03-25 11:16:54 -0400

    2.0.3 Updated Router with beforeEach function

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
-import Projects from '../views/Projects.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Projects from "../views/Projects.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: "/about",
+    name: "About",
+    component: About,
   },
   {
-    path: '/projects',
-    name: 'Projects',
-    component: Projects
-  }
-]
+    path: "/projects",
+    name: "Projects",
+    component: Projects,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-  scrollBehavior(to) {
-    if (to.hash) {
-      return {
-        el: to.hash,
-        behavior: 'smooth',
-      }
-    } else {
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve({ top: 0, left: 0 })
-        }, 250)
-      })
-    }
-  },
-})
+});
 
-export default router
+router.beforeEach(() => {
+  window.scrollTo(0, 0);
+});
+
+export default router;
